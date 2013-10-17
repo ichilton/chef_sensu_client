676ff08949b7716fa7da7e162a930f100bce913a
Ian Chilton <ian@ichilton.co.uk>
2013-10-17 15:45:03 +0100

    First commit of default recipe & attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,17 @@
 # Cookbook Name:: sensu_client
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
-#
+
+include_recipe 'sensu::default'
+
+sensu_client node.name do
+  address node[:ipaddress]
+  subscriptions node[:sensu_client][:subscriptions]
+  additional node[:sensu_client][:additional_attributes].to_hash
+end
+
+sensu_gem 'sensu-plugin' do
+  version node[:sensu_client][:sensu_plugin_version] if node[:sensu_client][:sensu_plugin_version]
+end
+
+include_recipe 'sensu::client_service'
