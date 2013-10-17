7c89435a9e1f08f93c27ec2f08d291e61803b7cd
Ian Chilton <ian@ichilton.co.uk>
2013-10-17 08:01:36 +0000

    Add dependancies and supported o/s to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,11 @@ license          'All rights reserved'
 description      'Installs/Configures sensu_client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+supports 'ubuntu'
+supports 'debian'
+supports 'centos'
+supports 'redhat'
+supports 'fedora'
+
+depends 'sensu'
